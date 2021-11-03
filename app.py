58641c64f1465a272288d5864d3fa51b8381b7e5
DineshNG <officialdng.1411@gmail.com>
2021-11-03 13:05:17 +0530

    formatting

--- app.py
+++ app.py
@@ -12,76 +12,93 @@ App config section
 """
 
 app = Flask(__name__)
-app.secret_key = 'this_is_my_key_of_secrets'
+app.secret_key = "this_is_my_key_of_secrets"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        username = request.form.get('username')
-        return redirect('/player/' + username)
-    return render_template('index.html')
+        username = request.form.get("username")
+        return redirect("/player/" + username)
+    return render_template("index.html")
 
 
-@app.route('/player/<string:username>', methods=['GET', 'POST'])
+@app.route("/player/<string:username>", methods=["GET", "POST"])
 def player(username):
     if isValid(username):
         pgn = latestGame(username)
         player_profile = playerProfile(username)
         categories, player_stats = playerStats(username)
-        return render_template('player.html', pgn=pgn, categories=categories, player_profile=player_profile,
-                               player_stats=player_stats)
+        return render_template(
+            "player.html",
+            pgn=pgn,
+            categories=categories,
+            player_profile=player_profile,
+            player_stats=player_stats,
+        )
     else:
         flash("Username not found", "warning")
-        return redirect('/')
+        return redirect("/")
 
-@app.route('/player/<string:username>/games',methods=['GET','POST'])
+
+@app.route("/player/<string:username>/games", methods=["GET", "POST"])
 def games(username):
     if request.method == "POST":
-        pgn=""
-        daymonth = request.form.get('daymonth')
+        pgn = ""
+        daymonth = request.form.get("daymonth")
         year = daymonth[0:4]
         month = daymonth[5:]
-        gamelist = gamesbyMonth(username,year,month)
+        gamelist = gamesbyMonth(username, year, month)
         gamelist.reverse()
-        month_string = datetime.datetime(int(year),int(month),1).strftime("%B")
-        return render_template('games.html', pgn=pgn, gamelist = gamelist,username=username,month=month_string,year=year)
+        month_string = datetime.datetime(int(year), int(month), 1).strftime("%B")
+        return render_template(
+            "games.html",
+            pgn=pgn,
+            gamelist=gamelist,
+            username=username,
+            month=month_string,
+            year=year,
+        )
 
 
-
-@app.route('/tactics')
+@app.route("/tactics")
 def tactics():
-    return render_template('tactics.html')
+    return render_template("tactics.html")
 
-@app.route('/leaderboards')
+
+@app.route("/leaderboards")
 def leaderboards():
-    return render_template('leaderboards.html')
+    return render_template("leaderboards.html")
 
-@app.route('/analyze',methods=['GET','POST'])
+
+@app.route("/analyze", methods=["GET", "POST"])
 def analyze():
-    if request.method == 'POST':
-        pgn = request.form.get('analyze')
-        return render_template('analyze.html',pgn=pgn)
+    if request.method == "POST":
+        pgn = request.form.get("analyze")
+        return render_template("analyze.html", pgn=pgn)
     else:
-        return render_template('analyze.html')
+        return render_template("analyze.html")
 
-@app.route('/board')
+
+@app.route("/board")
 def board():
-    return render_template('board.html')
+    return render_template("board.html")
 
-@app.route('/make_move', methods = ['POST'])
+
+@app.route("/make_move", methods=["POST"])
 def make_move():
     if sys.platform == "linux":
         os.chmod("./sf14", stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
         engine = chess.engine.SimpleEngine.popen_uci("./sf14")
     else:
         engine = chess.engine.SimpleEngine.popen_uci("./sf14.exe")
-    fen = request.form.get('data')
+    fen = request.form.get("data")
     board = chess.Board(fen)
-    info = engine.play(board,chess.engine.Limit(time=1.0))
+    info = engine.play(board, chess.engine.Limit(time=1.0))
     board.push(info.move)
     engine.quit()
     return board.fen()
 
-if __name__ == '__main__':
-    app.run(debug=True,threaded = True)
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
--- user.py
+++ user.py
@@ -1,5 +1,11 @@
-from chessdotcom import get_player_profile, get_player_stats, get_player_game_archives, get_player_games_by_month
+from chessdotcom import (
+    get_player_profile,
+    get_player_stats,
+    get_player_game_archives,
+    get_player_games_by_month,
+)
 import requests
+
 
 def playerProfile(username):
     """
@@ -8,8 +14,9 @@ def playerProfile(username):
     """
 
     response = get_player_profile(username)
-    player = response.json['player']
+    player = response.json["player"]
     return player
+
 
 def playerStats(username):
     """
@@ -19,22 +26,24 @@ def playerStats(username):
     'game': 'https://www.chess.com/game/live/26922645557'},
     'record': {'win': 660, 'loss': 530, 'draw': 67}}
     """
-    data = get_player_stats(username).json['stats']
+    data = get_player_stats(username).json["stats"]
     allcats = data.keys()
-    mylist = ['chess_rapid', 'chess_blitz', 'chess_bullet']
+    mylist = ["chess_rapid", "chess_blitz", "chess_bullet"]
     categories = []
     for cat in allcats:
         if cat in mylist:
             categories.append(cat)
     return categories, data
 
+
 def latestGame(username):
-    data = get_player_game_archives(username).json['archives']
+    data = get_player_game_archives(username).json["archives"]
     url = data[-1]
     games = requests.get(url).json()
-    game = games['games'][-1]
-    pgn = game['pgn']
+    game = games["games"][-1]
+    pgn = game["pgn"]
     return pgn
+
 
 def isValid(username):
     try:
@@ -43,31 +52,37 @@ def isValid(username):
     except:
         return False
 
+
 def gamesbyMonth(username, year, month):
+    """
+    dict_keys(
+        ['url','pgn','time_control','end_time','rated','tcn','uuid','initial_setup','fen','time_class','rules','white','black'])
+    """
     gamelist = []
     try:
         res = get_player_games_by_month(username, year, month)
-        data = res.json['games']
-        if (len(data) > 0):
+        data = res.json["games"]
+        print(data[0]["pgn"])
+        if len(data) > 0:
             for game in data:
                 game_dict = {}
                 result = "draw"
-                game_dict['pgn'] = game['pgn']
-                game_dict['time'] = game['time_class']
-                game_dict['white'] = game['white']['username']
-                game_dict['black'] = game['black']['username']
-                game_dict['url'] = game['url']
-                black_result = game['black']['result']
-                white_result = game['white']['result']
-                if game_dict['white'].casefold() == username.casefold():
-                    if white_result == 'win':
+                game_dict["pgn"] = game["pgn"]
+                game_dict["time"] = game["time_class"]
+                game_dict["white"] = game["white"]["username"]
+                game_dict["black"] = game["black"]["username"]
+                game_dict["url"] = game["url"]
+                black_result = game["black"]["result"]
+                white_result = game["white"]["result"]
+                if game_dict["white"].casefold() == username.casefold():
+                    if white_result == "win":
                         result = "win"
-                    elif black_result == 'win':
+                    elif black_result == "win":
                         result = "lost"
-                if game_dict['black'].casefold() == username.casefold():
-                    if black_result == 'win':
+                if game_dict["black"].casefold() == username.casefold():
+                    if black_result == "win":
                         result = "win"
-                    elif white_result == 'win':
+                    elif white_result == "win":
                         result = "lost"
                 if result == "win":
                     result_color = "#30a153cf"
@@ -75,7 +90,7 @@ def gamesbyMonth(username, year, month):
                     result_color = "#ff3333"
                 if result == "draw":
                     result_color = "#a5a5a5"
-                game_dict['result_color'] = result_color
+                game_dict["result_color"] = result_color
                 gamelist.append(game_dict)
         return gamelist
     except:
